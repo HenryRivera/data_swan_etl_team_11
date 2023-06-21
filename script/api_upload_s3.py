fdb279558ef1e8ad7755810c6af19c8b84023659
lucius12.2008@gmail.com <lucius12.2008@gmail.com>
2023-06-20 21:02:40 -0400

    updates to api_upload.py

--- script/api_upload_s3.py
+++ script/api_upload_s3.py
@@ -8,10 +8,9 @@ from pathlib import Path
 from botocore.exceptions import ClientError
 
 
-def get_ids(start, end,url):
+def get_ids(start:int, end:int, url:str) -> dict:
     years = [str(x) for x in range(start, end+1)]
     ids = {"General": [], "Research": [], "Ownership": []}
-    # url = 'https://openpaymentsdata.cms.gov/api/1/metastore/schemas/dataset/items?show-reference-ids'
     data = requests.get(url).json()
     for d in data:
         split_title = d['title'].split(" ")
@@ -21,16 +20,31 @@ def get_ids(start, end,url):
                 ids[split_title[1]].append(identifier)
     return ids 
 
-def get_datasets(id_list):
+def get_datasets(id_list:dict,url:str,dataset:str) -> pd.DataFrame:
     for category, category_ids in id_list.items():
         for category_id in category_ids:
-            url = 'https://openpaymentsdata.cms.gov/api/1/metastore/schemas/dataset/items/{}'.format(category_id)
-            data = requests.get(url).json()
+            url = url.format(category_id)
+            data = requests.get(''.join[url,category_id]).json()
             download = data["distribution"][0]["downloadURL"]
             try:
                 df = pd.read_csv(download, nrows=1)
             except Exception as e:
                 print("Failed to download:", download, "Error:", e)
+
+def get_datasets(distribution_ids:dict,request_url:str,dataset:str,limit:int,offset:int) -> pd.DataFrame:
+    if dataset == 'General':
+        general_distro_ids =  [id for id in distribution_ids['General']]
+        try:
+            for id in general_distro_ids:
+                requests.get(request_url.format(id,limit,offset))
+                
+        except Exception as e:
+            print (e)
+        pass
+    if dataset == 'Research':
+        pass 
+    if dataset == 'Ownership':
+        pass
 
 def create_S3_client():
     pass 
@@ -38,14 +52,11 @@ def create_S3_client():
 def upload_data_to_s3():
     create_S3_client()
     
-
 def main():
-    extract_api_main()
-    upload_data_to_s3()
-
-def extract_api_main():
     get_ids()
     get_datasets()
+    create_S3_client()
+    upload_data_to_s3()
 
 if __name__ == '__main__':
     main()
